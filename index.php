cfaf1a4361ae2932e3f963ceeb7392b2e5cae834
Montana <xavierpersonal404@gmail.com>
2024-05-08 22:41:58 -0600

    Agregado de tabla

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PRUEBA DE GIT</title>
+</head>
+<body>
+  <h1>Prueba git</h1>
+
+  <p>Esta es una prueba de como funciona git</p>
+
+  <table>
+    <tr>
+      <th>Usuario</th>
+      <th>Correo</th>
+    </tr>
+  </table>
+
+  <p>Aquí deberia ir una descripción</p>
+</body>
+</html>
