3f5994115b41c950b205de4bd9d7282946066db4
Montana <xavierpersonal404@gmail.com>
2024-05-12 17:51:17 -0600

    Color de fondo

--- index.php
+++ index.php
@@ -4,19 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PRUEBA DE GIT</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <h1>Prueba git</h1>
+  <h1> Modificando la pag</h1>
 
-  <p>Esta es una prueba de como funciona git</p>
-
-  <table>
-    <tr>
-      <th>Usuario</th>
-      <th>Correo</th>
-    </tr>
-  </table>
-
-  <p>Aquí deberia ir una descripción</p>
+  <p>Esta es una pagina modificada</p>
 </body>
 </html>
